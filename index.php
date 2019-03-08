d8edb3e58f800d22186243df741a0d9df3ec3126
118416564 <118416564@BISB93VL.bis.ad.ucc.ie>
2019-03-08 12:20:02 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>118416564</title>
+    <link rel="stylesheet" href="mystylesheet.css" type="text/css"/>
+    </head>
+    <body><h1 class="mainheading">118416564 Home!</h1>
+    <br/>
+    
+     <a href="index.html"> CV</a>
+     <br>
+     <br>
+     
+    <a href="https://github.com/AG-118416564/PhpProject1"> GitHub</a>
+    <br/>
+    <a href=""></a>
+    </body>
+</html>
